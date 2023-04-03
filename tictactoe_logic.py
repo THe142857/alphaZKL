ee916c7c36424499d8447c2a97968b2395dccf6f
ElianBelot <elian@belot.fr>
2023-04-03 13:20:57 -0400

    Added typing and refactored code

--- tictactoe_logic.py
+++ tictactoe_logic.py
@@ -1,4 +1,5 @@
 import numpy as np
+from typing import List, Tuple
 
 
 class Board:
@@ -10,32 +11,30 @@ class Board:
     def valid_moves(self):
         np.where(self.board == 0)
 
-    def move(self, move, p):
-        self.board[move[0], move[1]] = p
+    def move(self, move: Tuple[int, int], player: int):
+        """Takes in a board position and a player and updates the board"""
 
-    def has_won(self, p):
+        self.board[move[0], move[1]] = player
+
+    def has_won(self, player: int):
         # rows
         for i in range(self.n):
-            if np.all(self.board[i] == p):
+            if np.all(self.board[i] == player):
                 return True
 
         # cols
         for i in range(self.n):
-            if np.all(self.board[:][i] == p):
+            if np.all(self.board[:][i] == player):
                 return True
 
         # diag
         diag = self.board.diagonal()
         anti_diag = np.flip(self.board).diagonal()
-        if diag == p:
-            return True
-        if anti_diag == p:
-            return True
 
-        return False
+        return diag == player or anti_diag == player
 
     def __repr__(self):
-        res = ""
+        res = ''
         for i in range(2 * self.n + 1):
             if i % 2 == 0:
                 res += "-" * (2 * self.n - 1) + "\n"
@@ -44,7 +43,7 @@ class Board:
         return res
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     b = Board()
     b.move((2, 0), 1)
     print(b)
